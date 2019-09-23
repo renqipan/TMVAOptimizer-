916f67af6e021192e866504d41738de89b9c5be9
renqipan <renqiph@163.com>
2019-09-23 14:28:59 +0800

    train.c edit

--- src/TMVATrain.C
+++ src/TMVATrain.C
@@ -29,7 +29,7 @@ void TMVAAnalyzer::trainSample ( TTree * sample, int sampNo){
             _trainVars[varInd] = dummyVar;
             varInd++;
         }     
-        if( sampNo == sig ){
+        if( sampNo == ksig ){
             /* a specific weight can be given for a particular sample independent of the provided event weight in the tree. Change this weight (or comment it out accordingly) */ 
             weight = weight / TMath::Sqrt(pho1_sigmaEOverE*pho1_sigmaEOverE+pho2_sigmaEOverE*pho2_sigmaEOverE);
             if (i%5 == 0 || i%5 == 2 || i%5 == 4) _dataloaderTrain->AddSignalTrainingEvent( _trainVars, weight );
