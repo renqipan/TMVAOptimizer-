f5e34c2142d22f4382abd59faa41fcac615ae5a8
repan <renqiph@163.com>
2019-09-24 12:29:10 +0200

    color

--- histogram_plot.c
+++ histogram_plot.c
@@ -8,7 +8,7 @@ void histogram_plot(){
 	TH1F* test_S=(TH1F*) file1->Get("Method_BDT/BDT/MVA_BDT_S");
 	TH1F* test_B=(TH1F*) file1->Get("Method_BDT/BDT/MVA_BDT_B");
 	TH1F* powheg=(TH1F*)file2->Get("MVA_BDT_powheg");
-	train_S->SetLineColor(0);
+	train_S->SetLineColor(1);
 	train_B->SetLineColor(2);
 	test_S->SetLineColor(3);
 	test_B->SetLineColor(4);
@@ -18,7 +18,7 @@ void histogram_plot(){
 	
 	TCanvas* c= new TCanvas();
 	train_S->SetTitle("BDT output distribution;Response value;Density");
-	train_S->Draw("E");
+	train_S->Draw();
 	train_B->Draw("same");
 	test_S->Draw("sameHist");
 	test_B->Draw("sameHist");
