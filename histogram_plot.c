6dc6346b0119634d29ec0f2c429bc8264ee5dd20
repan <renqiph@163.com>
2019-09-25 16:29:19 +0200

    powheg

--- histogram_plot.c
+++ histogram_plot.c
@@ -1,7 +1,7 @@
 void histogram_plot(){
 	gStyle->SetOptStat(0);
 	TFile* file1=TFile::Open("/afs/cern.ch/user/r/repan/root_file/TMVA_had.root");
-	TFile* file2=TFile::Open("/afs/cern.ch/user/r/repan/CMSSW_9_4_0/src/TMVAOptimizer/tth_BDT_powheg_apply.root");
+	TFile* file2=TFile::Open("/afs/cern.ch/user/r/repan/CMSSW_9_4_0/src/TMVAOptimizer/tth_BDT_powheg_app.root");
 
 	TH1F* train_S=(TH1F*) file1->Get("Method_BDT/BDT/MVA_BDT_Train_S");
 	TH1F* train_B=(TH1F*) file1->Get("Method_BDT/BDT/MVA_BDT_Train_B");
@@ -22,7 +22,7 @@ void histogram_plot(){
 	train_B->DrawNormalized("same");
 	test_S->DrawNormalized("sameHist");
 	test_B->DrawNormalized("sameHist");
-	//powheg->Draw("same");
+	powheg->DrawNormalized("same");
 
 	TLegend leg(.7,.7,.9,.9);
 	leg.SetFillColor(0);
@@ -30,7 +30,7 @@ void histogram_plot(){
 	leg.AddEntry(train_B,"Train-odd");
 	leg.AddEntry(test_S,"Test-even");
 	leg.AddEntry(test_B,"Test-odd");
-	//leg.AddEntry(powheg,"apply-powheg");
+	leg.AddEntry(powheg,"apply-powheg");
 	leg.DrawClone("Same");
 
 	gPad->Print("hadronic_norl.png");
